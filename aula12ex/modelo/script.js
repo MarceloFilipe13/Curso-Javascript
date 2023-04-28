b0855f3d91d84053277c0b997f1b59935a670ee6
Marcelo <marcelo.filipe888@outlook.com>
2023-04-28 15:57:55 -0300

    aula12ex

---
+++ aula12ex/modelo/script.js
@@ -0,0 +1,19 @@
+/*let data = new Date();
+let hora = data.getHours();
+let minutos = data.getMinutes();
+let segundos = data.getSeconds();
+let saudacao;
+        
+if (hora >= 18) {
+    saudacao = "Boa noite";
+} else if (hora >= 12) {
+    saudacao = "Boa tarde";
+} else {
+    saudacao = "Bom dia";
+}
+
+hora = hora < 10 ? "0" + hora : hora;
+minutos = minutos < 10 ? "0" + minutos : minutos;
+segundos = segundos < 10 ? "0" + segundos : segundos;
+window.alert(saudacao + '!' + ' São exatamente: ' + hora + ":" + minutos + ":" + segundos);*/
+
