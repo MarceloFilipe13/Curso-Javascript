23dc836ce4a6352bb421bea1daf5f8d260692063
Marcelo <marcelo.filipe888@outlook.com>
2023-05-04 23:38:55 -0300

    updateex015

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,22 +1,3 @@
-/*let data = new Date();
-let hora = data.getHours();
-let minutos = data.getMinutes();
-let segundos = data.getSeconds();
-let saudacao;
-        
-if (hora >= 18) {
-    saudacao = "Boa noite";
-} else if (hora >= 12) {
-    saudacao = "Boa tarde";
-} else {
-    saudacao = "Bom dia";
-}
-
-hora = hora < 10 ? "0" + hora : hora;
-minutos = minutos < 10 ? "0" + minutos : minutos;
-segundos = segundos < 10 ? "0" + segundos : segundos;
-window.alert(saudacao + '!' + ' São exatamente: ' + hora + ":" + minutos + ":" + segundos);*/
-
 function carregar() {
     let msg = window.document.getElementById('msg')
     let img = window.document.getElementById('imagem')
